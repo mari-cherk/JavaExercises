34be1326a01ed7e94fac9f458cb53e8adfa14685
Marina Cherkasova <1mari.cherkasova@gmail.com>
2020-08-10 16:23:54 +0300

    Methods

---
+++ src/main/java/MethodsHomeWork.java
@@ -0,0 +1,114 @@
+public class MethodsHomeWork {
+
+    public static void main(String[] args) {
+
+        //printStars(6);
+        //printSquare(6);
+        //printRectangle(17, 3);
+        //printTriangle(4);
+        //printRightAlignedTriangle(4);
+        xmasTree(10);
+    }
+
+    private static void printStars(int amount){
+        int i = 0;
+        while(i < amount){
+            System.out.print("*");
+            i++;
+        }
+    }
+
+    private static void printSquare(int amount){
+        int j = 0;
+        while(j < amount) {
+            int i = 0;
+            while (i < amount) {
+                System.out.print("*");
+                i++;
+            }
+            System.out.print("\n");
+            j++;
+        }
+    }
+
+    private static void printRectangle(int width, int height){
+        int j = 0;
+        while(j < height) {
+            int i = 0;
+            while (i < width) {
+                System.out.print("*");
+                i++;
+            }
+            System.out.print("\n");
+            j++;
+        }
+    }
+
+    private static void printTriangle(int size){
+        int j = 0;
+        int s = 1;
+        while(j < size) {
+            int i = 0;
+            while (i < s) {
+                System.out.print("*");
+                i++;
+            }
+            s++;
+            System.out.print("\n");
+            j++;
+        }
+    }
+
+    private static void printRightAlignedTriangle(int size){
+        int j = 0;
+        int spaces = size - 1;
+        while(j < size) {
+            int i = 0;
+            while (i < spaces) {
+                System.out.print(" ");
+                i++;
+            }
+            while (i < size){
+                System.out.print("*");
+                i++;
+            }
+            spaces--;
+            System.out.print("\n");
+            j++;
+        }
+    }
+
+    private static void xmasTree(int height){
+        int j = 0;
+        int h = height;
+        int spaces = height - 1;
+        while(j < height) {
+            int i = 0;
+            while (i < spaces) {
+                System.out.print(" ");
+                i++;
+            }
+            while (i < h){
+                System.out.print("*");
+                i++;
+            }
+            h++;
+            spaces--;
+            System.out.print("\n");
+            j++;
+        }
+        int stand = 0;
+        while (stand < 2){
+            int i = 0;
+            while (i < height - 2){
+                System.out.print(" ");
+                i++;
+            }
+            System.out.print("*");
+            System.out.print("*");
+            System.out.print("*");
+            stand++;
+            System.out.print("\n");
+        }
+    }
+}
