d9f371b65346041674480e8701caa6cf0fac2d79
Marina Cherkasova <1mari.cherkasova@gmail.com>
2020-08-12 23:08:29 +0200

    Cash Register

--- src/main/java/Objects/objectAsMmethodParameter/MainClass.java
+++ src/main/java/Objects/objectAsMmethodParameter/MainClass.java
@@ -3,7 +3,7 @@ package Objects.objectAsMmethodParameter;
 public class MainClass {
     public static void main(String[] args) {
 
-        Reformatory eastHelsinkiReformatory = new Reformatory();
+        /*Reformatory eastHelsinkiReformatory = new Reformatory();
 
         Person brian = new Person("Brian", 1, 110, 7);
         Person pekka = new Person("Pekka", 33, 176, 85);
@@ -20,6 +20,65 @@ public class MainClass {
         eastHelsinkiReformatory.weight(brian);
         eastHelsinkiReformatory.weight(brian);
 
-        System.out.println("total weights measured "+eastHelsinkiReformatory.totalWeightsMeasured());
+        System.out.println("total weights measured "+eastHelsinkiReformatory.totalWeightsMeasured());*/
+
+        /*LyyraCard cardOfPekka = new LyyraCard(10);
+
+        System.out.println("money on the card " + cardOfPekka.balance() );
+        boolean succeeded = cardOfPekka.pay(8);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );
+
+        succeeded = cardOfPekka.pay(4);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );*/
+
+        /*CashRegister unicafeExactum = new CashRegister();
+
+        double theChange = unicafeExactum.payEconomical(10);
+        System.out.println("the change was " + theChange );
+
+        theChange = unicafeExactum.payEconomical(5);
+        System.out.println("the change was "  + theChange );
+
+        theChange = unicafeExactum.payGourmet(4);
+        System.out.println("the change was "  + theChange );
+
+        System.out.println( unicafeExactum );*/
+
+        /*CashRegister unicafeExactum = new CashRegister();
+
+        double theChange = unicafeExactum.payEconomical(10);
+        System.out.println("the change was " + theChange );
+
+        LyyraCard cardOfJim = new LyyraCard(7);
+
+        boolean succeeded = unicafeExactum.payGourmet(cardOfJim);
+        System.out.println("payment success: " + succeeded);
+        succeeded = unicafeExactum.payGourmet(cardOfJim);
+        System.out.println("payment success: " + succeeded);
+        succeeded = unicafeExactum.payEconomical(cardOfJim);
+        System.out.println("payment success: " + succeeded);
+
+        System.out.println( unicafeExactum );*/
+
+        CashRegister unicafeExactum = new CashRegister();
+        System.out.println( unicafeExactum );
+
+        LyyraCard cardOfJim = new LyyraCard(2);
+
+        System.out.println("the card balance " + cardOfJim.balance() + " euros");
+
+        boolean succeeded = unicafeExactum.payGourmet(cardOfJim);
+        System.out.println("payment success: " + succeeded);
+
+        unicafeExactum.loadMoneyToCard(cardOfJim, 100);
+
+        succeeded = unicafeExactum.payGourmet(cardOfJim);
+        System.out.println("payment success: " + succeeded);
+
+        System.out.println("the card balance " + cardOfJim.balance() + " euros");
+
+        System.out.println( unicafeExactum );
     }
 }
