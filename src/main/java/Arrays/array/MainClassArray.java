c2a9229f94fd8805d1bf064d99513229caf1d3da
Marina Cherkasova <1mari.cherkasova@gmail.com>
2020-08-13 16:37:51 +0300

    Arrays

---
+++ src/main/java/Arrays/array/MainClassArray.java
@@ -0,0 +1,21 @@
+package Arrays.array;
+
+import java.util.Arrays;
+
+public class MainClassArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {5, 1, 3, 4, 2};
+        System.out.println(sum(array));
+        printElegantly(array);
+    }
+
+    public static int sum(int[] array) {
+        return Arrays.stream(array).reduce(0, (int a, int b) -> a + b);
+    }
+
+    public static void printElegantly(int[] array){
+        Arrays.stream(array).forEach(x -> System.out.print(x + ", "));
+    }
+}
